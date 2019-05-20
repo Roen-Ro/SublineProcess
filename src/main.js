6e5226932c92ac80fca215b64da12831d615803d
lolaage <lolaage@qingqingcaodeMacBook-Pro.local>
2019-05-20 08:41:14 +0800

    add resources

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-
+//https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/String JS字符处理
 process.env.NODE_ENV = 'development';//部署的时候设置成 production
 
 var inputPara = process.argv;//输入的参数 数组，注意参数是从第3个元素开始的（index为2），第一个元素为node执行器目录地址，第二个为js执行文件地址
@@ -30,21 +30,35 @@ function parseSrtFromFile(path) {
 }
 
 console.log('inputed parameters are: '+ inputPara);
-var srtPath = inputPara[2];
+var srtPath = '/Users/lolaage/Desktop/subtitles/InTheHeartOfTheSun/ChineseSimplified-test.srt';//inputPara[2];
 
-// const rl = readline.createInterface({
-//  // input: fs.createReadStream(srtPath) //直接读取文件流，有可能会遇到编码问题，所以input要为解码后的字符
+const rl = readline.createInterface({
+  input: fs.createReadStream(srtPath) //直接读取文件流，有可能会遇到编码问题，所以input要为解码后的字符
+});
+
+var isLastBlankLine = true;
+rl.on('line', (line) => {
+
+ 
+  if(isLastBlankLine){
+    console.log('----------------------');
+  }
+
+  console.log(`${line}`);
+
+  let len = line.trim().length;
+  if(len == 0)
+    isLastBlankLine = true;
+  else
+    isLastBlankLine = false;
+   
+});
+
+
+// parseSrtFromFile(srtPath)
+// .then((srts) => {
+//   console.log(srts);
+// })
+// .catch( err => { //这样写的话，外部永远都不会抓取到异常了，但是!这对通过 await 调用就很方便了, 永远只需要拿返回值，然后通过返回值的error字段，判断是否有异常
+//   console.error('parseSrtFromFile error: ' + err.message);
 // });
-
-// rl.on('line', (line) => {
-//   console.log(`Line：${line}`);
-// });
-
-
-parseSrtFromFile(srtPath)
-.then((srts) => {
-  console.log(srts);
-})
-.catch( err => { //这样写的话，外部永远都不会抓取到异常了，但是!这对通过 await 调用就很方便了, 永远只需要拿返回值，然后通过返回值的error字段，判断是否有异常
-  console.error('parseSrtFromFile error: ' + err.message);
-});;
